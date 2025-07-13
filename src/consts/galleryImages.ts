980dd490ad7e9d3d4eff3b30b6b39a3cccd9dac8
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-07-13 16:20:30 -0500

    First commitment: initial structure of Believe and Create

---
+++ src/consts/galleryImages.ts
@@ -0,0 +1,207 @@
+import GalleryImg1 from "@/assets/images/gallery/gallery_01.webp"
+import GalleryImg2 from "@/assets/images/gallery/gallery_02.webp"
+import GalleryImg3 from "@/assets/images/gallery/gallery_03.webp"
+import GalleryImg5 from "@/assets/images/gallery/gallery_05.webp"
+import GalleryImg6 from "@/assets/images/gallery/gallery_06.webp"
+import GalleryImg7 from "@/assets/images/gallery/gallery_07.webp"
+import GalleryImg8 from "@/assets/images/gallery/gallery_08.webp"
+import GalleryImg9 from "@/assets/images/gallery/gallery_09.webp"
+import GalleryImg10 from "@/assets/images/gallery/gallery_10.webp"
+import GalleryImg11 from "@/assets/images/gallery/gallery_11.webp"
+import GalleryImg12 from "@/assets/images/gallery/gallery_12.webp"
+import GalleryImg13 from "@/assets/images/gallery/gallery_13.webp"
+import GalleryImg14 from "@/assets/images/gallery/gallery_14.webp"
+import GalleryImg15 from "@/assets/images/gallery/gallery_15.webp"
+import GalleryImg16 from "@/assets/images/gallery/gallery_16.webp"
+import GalleryImg17 from "@/assets/images/gallery/gallery_17.webp"
+import GalleryImg18 from "@/assets/images/gallery/gallery_18.webp"
+import GalleryImg19 from "@/assets/images/gallery/gallery_19.webp"
+import GalleryImg20 from "@/assets/images/gallery/gallery_20.webp"
+import GalleryImg21 from "@/assets/images/gallery/gallery_21.webp"
+import GalleryImg22 from "@/assets/images/gallery/gallery_22.webp"
+import GalleryImg23 from "@/assets/images/gallery/gallery_23.webp"
+import GalleryImg24 from "@/assets/images/gallery/gallery_24.webp"
+import GalleryImg25 from "@/assets/images/gallery/gallery_25.webp"
+import GalleryImg26 from "@/assets/images/gallery/gallery_26.webp"
+import GalleryImg27 from "@/assets/images/gallery/gallery_27.webp"
+import GalleryImg28 from "@/assets/images/gallery/gallery_28.webp"
+import GalleryImg29 from "@/assets/images/gallery/gallery_29.webp"
+import GalleryImg30 from "@/assets/images/gallery/gallery_30.webp"
+import GalleryImg31 from "@/assets/images/gallery/gallery_31.webp"
+import GalleryImg32 from "@/assets/images/gallery/gallery_32.webp"
+import GalleryImg33 from "@/assets/images/gallery/gallery_33.webp"
+import GalleryImg34 from "@/assets/images/gallery/gallery_34.webp"
+import GalleryImg35 from "@/assets/images/gallery/gallery_35.webp"
+import GalleryImg36 from "@/assets/images/gallery/gallery_36.webp"
+import GalleryImg37 from "@/assets/images/gallery/gallery_37.webp"
+
+export const galleryImages = [
+  {
+    image: GalleryImg1,
+    alt: "Lola Lolita en un escenario con Carlos Baute.",
+    thumb: "thumb1",
+  },
+  {
+    image: GalleryImg2,
+    alt: "Lola Lolita Land, festival de música.",
+    thumb: "thumb2",
+  },
+  {
+    image: GalleryImg3,
+    alt: "Lola Índigo en un escenario en el festival de Lola Lolita Land.",
+    thumb: "thumb3",
+  },
+  {
+    image: GalleryImg5,
+    alt: "Lola Lolita junto a un grupo de bailarines.",
+    thumb: "thumb4",
+  },
+  {
+    image: GalleryImg6,
+    alt: "Lola Lolita junto a un grupo de bailarines.",
+    thumb: "thumb5",
+  },
+  {
+    image: GalleryImg7,
+    alt: "Lola Lolita junto a un grupo de bailarines.",
+    thumb: "thumb6",
+  },
+  {
+    image: GalleryImg8,
+    alt: "Lola Lolita junto a un grupo de bailarines.",
+    thumb: "thumb7",
+  },
+  {
+    image: GalleryImg9,
+    alt: "Lola Lolita junto a un grupo de bailarines.",
+    thumb: "thumb8",
+  },
+  {
+    image: GalleryImg10,
+    alt: "Lola Lolita junto a un grupo de bailarines.",
+    thumb: "thumb9",
+  },
+  {
+    image: GalleryImg11,
+    alt: "Marina Reche en el evento de Lola Lolita Land.",
+    thumb: "thumb10",
+  },
+  {
+    image: GalleryImg12,
+    alt: "Marina Reche en el evento de Lola Lolita Land.",
+    thumb: "thumb11",
+  },
+  {
+    image: GalleryImg13,
+    alt: "DJ en el evento de Lola Lolita Land.",
+    thumb: "thumb12",
+  },
+  {
+    image: GalleryImg14,
+    alt: "DJ en el evento de Lola Lolita Land.",
+    thumb: "thumb13",
+  },
+  {
+    image: GalleryImg15,
+    alt: "Grupo de personas con tambores sobre un escenario.",
+    thumb: "thumb14",
+  },
+  {
+    image: GalleryImg16,
+    alt: "Grupo de personas con tambores sobre un escenario.",
+    thumb: "thumb15",
+  },
+  {
+    image: GalleryImg17,
+    alt: "Gente mirando hacia el escenario.",
+    thumb: "thumb16",
+  },
+  {
+    image: GalleryImg18,
+    alt: "Gente mirando hacia el escenario.",
+    thumb: "thumb17",
+  },
+  {
+    image: GalleryImg19,
+    alt: "Lola Lolita en un escenario cantando.",
+    thumb: "thumb18",
+  },
+  {
+    image: GalleryImg20,
+    alt: "Lola Lolita en un escenario bailando.",
+    thumb: "thumb19",
+  },
+  {
+    image: GalleryImg21,
+    alt: "Lola Lolita en un escenario bailando junto a Abraham Mateo cantando.",
+    thumb: "thumb20",
+  },
+  {
+    image: GalleryImg22,
+    alt: "Dos personas lanzando regalos al público.",
+    thumb: "thumb21",
+  },
+  {
+    image: GalleryImg23,
+    alt: "Lola Lolita dándose un abrazo con sus fans.",
+    thumb: "thumb22",
+  },
+  { image: GalleryImg24, alt: "DJ en el evento.", thumb: "thumb23" },
+  {
+    image: GalleryImg25,
+    alt: "Gente de fiesta en el evento de Lola Lolita.",
+    thumb: "thumb24",
+  },
+  {
+    image: GalleryImg26,
+    alt: "Lola Lolita saliendo de una caja sorpresa.",
+    thumb: "thumb25",
+  },
+  {
+    image: GalleryImg27,
+    alt: "Gente sobre el escenario bailando y rapeando.",
+    thumb: "thumb26",
+  },
+  {
+    image: GalleryImg28,
+    alt: "Lola Lolita cantando y bailando junto a otras personas.",
+    thumb: "thumb27",
+  },
+  { image: GalleryImg29, alt: "Lola Lolita Land.", thumb: "thumb28" },
+  {
+    image: GalleryImg30,
+    alt: "Lola Lolita cantando sobre un escenario con un traje azul.",
+    thumb: "thumb29",
+  },
+  {
+    image: GalleryImg31,
+    alt: "Lola Lolita celebrando junto a Carlos Baute.",
+    thumb: "thumb30",
+  },
+  {
+    image: GalleryImg32,
+    alt: "Lola Lolita tomándose una foto con un grupo de personas.",
+    thumb: "thumb31",
+  },
+  {
+    image: GalleryImg33,
+    alt: "Lola Índigo y otras dos chicas cantando y bailando sobre el escenario.",
+    thumb: "thumb32",
+  },
+  {
+    image: GalleryImg34,
+    alt: "Lola Índigo y otras dos chicas cantando y bailando sobre el escenario.",
+    thumb: "thumb33",
+  },
+  {
+    image: GalleryImg35,
+    alt: "Lola Índigo y otras dos chicas cantando y bailando sobre el escenario.",
+    thumb: "thumb34",
+  },
+  {
+    image: GalleryImg36,
+    alt: "El público mirando la coreografía de Lola Lolita junto a su grupo.",
+    thumb: "thumb35",
+  },
+  { image: GalleryImg37, alt: "Imagen de la galería 37.", thumb: "thumb36" },
+]
